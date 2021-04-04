b0a5838cee3a16f7beeb5ac878f6fcacfe6ab9e7
David Whittington <djwhitt@gmail.com>
2021-04-04 13:26:56 -0500

    Remove non-package code
    Switching to this flake to being just a package.

--- flake.nix
+++ flake.nix
@@ -1,90 +1,51 @@
 {
-  description = "Arweave Server flake";
+  description = "Arweave server binary package flake";
 
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in rec {
+      packages.x86_64-linux.arweave-bin =
+        let
+          p = { stdenv, lib, fetchurl, autoPatchelfHook, zlib, gmp, openssl, ncurses, ... }: pkgs.stdenv.mkDerivation rec {
+            name = "arweave-bin";
+            version = "2.4.1.0";
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      rec {
-        packages = flake-utils.lib.flattenTree {
-          arweave-bin =
-            let
-              p = { stdenv, lib, fetchurl, autoPatchelfHook, zlib, gmp, openssl, ncurses, ... }: pkgs.stdenv.mkDerivation rec {
-                name = "arweave-bin";
-                version = "2.4.1.0";
+            src = fetchurl {
+              url = "https://github.com/ArweaveTeam/arweave/releases/download/N.${version}/arweave-${version}.centos8-ubuntu20-x86_64.tar.gz";
+              sha256 = "sha256-SxHz1y0/Hph+HvEmujjyHZHo/tDP/XFByGRoJfZ6Sss=";
+            };
 
-                src = fetchurl {
-                  url = "https://github.com/ArweaveTeam/arweave/releases/download/N.${version}/arweave-${version}.centos8-ubuntu20-x86_64.tar.gz";
-                  sha256 = "sha256-SxHz1y0/Hph+HvEmujjyHZHo/tDP/XFByGRoJfZ6Sss=";
-                };
+            nativeBuildInputs = [
+              autoPatchelfHook
+              stdenv.cc.cc.lib
+              gmp
+              ncurses
+              openssl
+              zlib
+            ];
 
-                nativeBuildInputs = [
-                  autoPatchelfHook
-                  stdenv.cc.cc.lib
-                  gmp
-                  ncurses
-                  openssl
-                  zlib
-                ];
+            unpackPhase = ''
+              tar xvf $src
+            '';
 
-                unpackPhase = ''
-                  tar xvf $src
-                '';
+            installPhase = ''
+              mkdir $out
+              mv * $out/
+              cat << EOH > $out/releases/2.4.1.0/vm.args
+              -name arweave@127.0.0.1
+              -setcookie arweave
+              EOH
+            '';
 
-                installPhase = ''
-                  mkdir -p $out/arweave
-                  mv * $out/arweave
-                '';
-
-                meta = with lib; {
-                  homepage = https://github.com/ArweaveTeam/arweave;
-                  description = "Arweave Server.";
-                  platforms = platforms.linux;
-                };
-              };
-            in pkgs.callPackage p { };
-        };
-
-        defaultPackage = packages.arweave-bin;
-
-        nixosModule = {
-          options = {
-            services.arweave = {
-              enable = pkgs.lib.mkOption {
-                type = pkgs.lib.types.bool;
-                default = false;
-                description = ''
-                  Whether to run Arweave Server.
-                '';
-              };
+            meta = with lib; {
+              homepage = https://github.com/ArweaveTeam/arweave;
+              description = "Arweave Server.";
+              platforms = platforms.linux;
             };
           };
+        in pkgs.callPackage p { };
 
-          config = pkgs.lib.mkIf pkgs.lib.config.services.arweave.enable {
-
-            environment.systemPackages = [ pkgs.arweave-bin ];
-
-            systemd.services.arweave = {
-              description = "Arweave Server";
-              wantedBy = [ "multi-user.target" ];
-
-              serviceConfig = {
-                User = "arweave";
-                Group = "arweave";
-                ExecStart = "${pkgs.arweave-bin}/arweave/bin/start";
-                ExecStop = "${pkgs.arweave-bin}/arweave/bin/stop";
-              };
-            };
-
-            users.users.arweave = {
-              description = "Arweave Server user";
-              group = "arweave";
-            };
-          };
-        };
-      }
-    );
+      defaultPackage.x86_64-linux = packages.x86_64-linux.arweave-bin;
+    };
 }
